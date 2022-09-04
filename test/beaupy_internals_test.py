18595fdc83ffadcc21583a8ad0fa39fb7f2c0626
Peter Vyboch <pvyboch1@gmail.com>
2022-09-04 10:47:42 +0200

    test: internals test

--- test/beaupy_internals_test.py
+++ test/beaupy_internals_test.py
@@ -1,15 +1,15 @@
 from ward import test
 
-from beaupy._internals import __render_prompt
+from beaupy._internals import _render_prompt
 
 
 @test("Test that prompt is rendered properly")
 def _():
-    result = __render_prompt(False, ["a", "b"], "Test prompt", 1, None)
+    result = _render_prompt(False, ["a", "b"], "Test prompt", 1, None)
     assert result == "Test prompt\n> a[black on white]b[/black on white] "
 
 
 @test("Test that prompt is rendered with error")
 def _():
-    result = __render_prompt(False, ["a", "b"], "Test prompt", 1, "Test Error")
+    result = _render_prompt(False, ["a", "b"], "Test prompt", 1, "Test Error")
     assert result == "Test prompt\n> a[black on white]b[/black on white] \n[red]Error:[/red] Test Error"
