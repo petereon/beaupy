12582fbd6e929b3bfe69176e479101ce947f5cf1
petereon <pvyboch1@gmail.com>
2023-03-26 09:47:30 +0200

    chore: linting and formatting

--- test/beaupy_internals_test.py
+++ test/beaupy_internals_test.py
@@ -17,17 +17,19 @@ def _():
         result == "Test prompt\n> a[black on white]b[/black on white] \n\n(Confirm with [bold]enter[/bold])\n[red]Error:[/red] Test Error"
     )
 
+
 @test("Test that abort prints printable key correctly")
 def _():
-    key = Key("test", (1,2,3), is_printable=True)
+    key = Key("test", (1, 2, 3), is_printable=True)
     try:
         raise Abort(key)
     except Abort as e:
         assert str(e) == "Aborted by user with key test"
 
+
 @test("Test that abort prints non-printable key correctly")
 def _():
-    key = Key("test", (1,2,3), is_printable=False)
+    key = Key("test", (1, 2, 3), is_printable=False)
     try:
         raise Abort(key)
     except Abort as e:
